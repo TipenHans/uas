94b3a1f4222a40a378fb50e2bd726d8c5d797f46
TipenHans <167948766+TipenHans@users.noreply.github.com>
2024-05-19 21:27:05 +0700

    Delete src directory

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-import Main from "./Main";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<Main />);
