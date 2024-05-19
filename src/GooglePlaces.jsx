1771444de2d4b4683934e8825bd7f54e117fa124
TipenHans <167948766+TipenHans@users.noreply.github.com>
2024-05-20 00:00:26 +0700

    Update GooglePlaces.jsx

--- src/GooglePlaces.jsx
+++ src/GooglePlaces.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { API_KEY } from './APIKey.js';
 
 function GooglePlaces() {
   const [selectedPlace, setSelectedPlace] = useState(null);
@@ -8,7 +9,7 @@ function GooglePlaces() {
     const loadScript = () => {
       const script = document.createElement("script");
       script.src =
-        "https://maps.googleapis.com/maps/api/js?key=AIzaSyAgphDeswrhslHC1PoAcOpwBJqdCyO242M&libraries=places";
+        "https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places";
       script.async = true;
       script.defer = true;
       script.onload = initAutocomplete;
